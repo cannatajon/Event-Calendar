946a4f6068bdbb5478f7a5840653a936136325d3
Jon Cannata <joncannats@gmail.com>
2022-04-01 19:18:31 -0400

    add calendar grid view

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path('gridview/', views.CalendarView.as_view(), name='grid_view')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,41 @@
 from urllib import response
 from django.shortcuts import render, redirect
+from datetime import date, datetime
+from django.http import HttpResponse
+from django.views import generic
+from django.utils.safestring import mark_safe
+from .models import *
+from .utils import Calendar
 
 # Create your views here.
 
 
 def home(req):
     return render(req, "home.html")
+
+
+def grid_view(req):
+    return render(req, 'grid_view.html')
+
+
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'grid_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        d = get_date(self.request.GET.get('day', None))
+
+        cal = Calendar(d.year, d.month)
+
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
