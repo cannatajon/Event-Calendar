cdd7ddd2f446d719e8216ee9e3ecf5736fd9d2ed
Jonathan Toye <jontoye@gmail.com>
2022-04-02 23:45:08 -0400

    update db models

--- main_app/models.py
+++ main_app/models.py
@@ -3,8 +3,22 @@ from django.db import models
 # Create your models here.
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Venue(models.Model):
+    eventbrite_id = models.CharField(max_length=16)
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+
+
 class Event(models.Model):
-    title = models.CharField(max_length=200)
+    eventbrite_id = models.CharField(max_length=16, blank=True)
+    title = models.TextField()
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    image = models.URLField(max_length=500, blank=True)
+    tags = models.ManyToManyField(Tag)
+    venue = models.ManyToManyField(Venue)
