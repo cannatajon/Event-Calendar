4e7639231241001103e6fdfaf813808f23d70cfb
Jonathan Toye <jontoye@gmail.com>
2022-04-06 16:25:59 -0400

    update pin view page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,13 +5,14 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('gridview/', views.CalendarView.as_view(), name='grid_view'),
-    path('search/', views.search, name='search'),
+    path('pinview/', views.pin_view, name="pin_view"),
 
+    path('search/', views.search, name='search'),
 
     path('events/create', views.event_create, name="event_create"),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'),
-    path('events/<int:event_id>/addtocalendar',views.add_to_calendar, name='add_to_calendar'),
-    path('events/<int:event_id>/removefromcalendar',views.remove_from_calendar, name='remove_from_calendar'),
+    path('events/<int:event_id>/addtocalendar', views.add_to_calendar, name='add_to_calendar'),
+    path('events/<int:event_id>/removefromcalendar', views.remove_from_calendar, name='remove_from_calendar'),
     path('events/addevent', views.add_event, name='add_event'),
 
 
