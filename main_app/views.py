6212f7888124fd51ac60c353e7d3cebea8ffda7c
Jon Cannata <joncannats@gmail.com>
2022-04-06 16:38:57 -0400

    delete events

--- main_app/views.py
+++ main_app/views.py
@@ -273,6 +273,11 @@ class DeleteUser(LoginRequiredMixin, DeleteView):
     success_url = '/'
 
 
+class DeleteEvent(LoginRequiredMixin, DeleteView):
+    model = Event
+    success_url = '/'
+
+
 class editProfile(LoginRequiredMixin, UpdateView):
     model = Profile
     fields = ['profile_pic', 'bio']
